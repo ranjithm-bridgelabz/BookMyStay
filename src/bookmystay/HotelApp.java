6154d5389c309eec5f8215fa23483e8acdacad70
ranjithm-bridgelabz <ranjith.muthukrishnan@bridgelabz.com>
2026-06-18 11:23:46 +0530

    feat: implement room inventory setup

--- src/bookmystay/HotelApp.java
+++ src/bookmystay/HotelApp.java
@@ -2,6 +2,7 @@ package bookmystay;
 
 public class HotelApp {
     public static void main(String[] args) {
+        
         System.out.println("Welcome to BookMyStay App!");
         
         System.out.println("\n--- [UC1: Initializing Inventory] ---");
